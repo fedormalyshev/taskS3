f3308ba6a2540a796ac34dedb5fa06ee2c206d61
fedormalyshev <43319121+fedormalyshev@users.noreply.github.com>
2018-09-16 22:34:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import logging
-#import pickle
 import requests
 import pandas as pd
 from sklearn.externals import joblib
@@ -10,14 +9,14 @@ app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
+response = requests.get('https://www.dropbox.com/s/f3zomahc0ri9q4j/VALUEreg.pkl?dl=1')
+with open('VALUE_model', 'wb') as file1:
+    file1.write(response.content)
+    
+VALUE_model = joblib.load('VALUE_model')
+
 @app.route("/predict")
 def predict():
-    
-    response = requests.get('https://www.dropbox.com/s/f3zomahc0ri9q4j/VALUEreg.pkl?dl=1')
-    with open('VALUE_model', 'wb') as file1:
-        file1.write(response.content)
-    
-    VALUE_model = joblib.load('VALUE_model')
     
     csv_url = request.args.get('csv_url')
     one_row = pd.read_csv("https://www.dropbox.com/s/39dak20mpjmss0q/1_row_test.csv?dl=1")
@@ -25,8 +24,6 @@ def predict():
     
     #return(csv_url)
     return(str(x))
-    #i = request.args.get('i')
-    #print("\n", i)
 
 if __name__ == "__main__":
     #port = int(os.environ.get("VCAP_APP_PORT", 5000))
