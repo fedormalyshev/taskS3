87334daf36616551b2ceaf23cdf569eacb8f7fa6
fedormalyshev <43319121+fedormalyshev@users.noreply.github.com>
2018-09-16 21:28:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,11 @@ import pandas as pd
 from sklearn.externals import joblib
 from flask import Flask, jsonify, request
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 @app.route("/predict")
 def predict():
-    response = requests.get('https://www.dropbox.com/s/f3zomahc0ri9q4j/VALUEreg.pkl?dl=1')
-    with open('VALUE_model', 'wb') as file1:
-        file1.write(response.content)
-    
     csv_url = request.args.get('csv_url')
     
     #one_row = pd.read_csv("https://www.dropbox.com/s/39dak20mpjmss0q/1_row_test.csv?dl=1")
@@ -22,10 +20,12 @@ def predict():
     #print("\n", i)
 
 if __name__ == "__main__":
-        
+    response = requests.get('https://www.dropbox.com/s/f3zomahc0ri9q4j/VALUEreg.pkl?dl=1')
+    with open('VALUE_model', 'wb') as file1:
+        file1.write(response.content)
+    
     #VALUE_model = joblib.load('VALUE_model')
     
     #port = int(os.environ.get("VCAP_APP_PORT", 5000))
     #app.run(host='0.0.0.0', port=port)
-    app.debug = True
     app.run()
